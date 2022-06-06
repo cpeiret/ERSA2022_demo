45fd20d2fa125caf3a69070b434886f37a2deedb
Clara Peiret-Garcia <c.peiret-garcia2@newcastle.ac.uk>
2022-06-06 15:06:48 +0100

    gwr test

--- 03_accessibility.r
+++ 03_accessibility.r
@@ -32,8 +32,8 @@ r5r_core <- setup_r5('./data/pbf')
 
 # set inputs
 mode <- c("WALK")
-max_walk_dist <- 1125
-max_trip_duration <- 15
+max_walk_dist <- 800
+max_trip_duration <- 10
 departure_datetime <- as.POSIXct("13-05-2019 14:00:00",
                                  format = "%d-%m-%Y %H:%M:%S")
 
@@ -50,13 +50,43 @@ ttm <- travel_time_matrix(r5r_core = r5r_core,
 head(ttm)
 
 # accessibility score based on how many points can be accessed within 20 min
-access_score = as.data.frame(table(ttm$from_id))
+access_score_seniors = as.data.frame(table(ttm$from_id))
 
 # merge access score with polygons layer
 hexagons = st_read_parquet('./data/hexagons.parquet') # load polygons layer
-hexagons = merge(hexagons, access_score, by.x = 'hex_id', by.y = 'Var1') # merge
-hexagons = hexagons %>% rename('access_adults' = 'Freq') # rename variable
+hexagons = merge(hexagons, access_score_seniors, by.x = 'hex_id', by.y = 'Var1') # merge
+hexagons = hexagons %>% rename('access_seniors' = 'Freq') # rename variable
 st_write_parquet(hexagons, './results/hexagons_access.parquet') # save to parquet
+
+# detailed itineraries
+dit <- detailed_itineraries(r5r_core = r5r_core,
+                            origins = centroids[27,],
+                            destinations = centroids[25,],
+                            mode = 'WALK',
+                            departure_datetime = departure_datetime,
+                            max_walk_dist = 10000,
+                            shortest_path = FALSE,
+                            verbose = FALSE)
+head(dit)
+
+# extract OSM network
+street_net <- street_network_to_sf(r5r_core)
+
+# plot
+ggplot() +
+  geom_sf(data = street_net$edges, color='gray85') +
+  geom_sf(data = dit, aes(color=mode)) +
+  facet_wrap(.~option) + 
+  theme_void()
+
+
+
+
+
+
+
+
+
 
 
 stop_r5(r5r_core)
